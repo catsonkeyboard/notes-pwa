a6ca67128c53b8e540ff5eb1566e6aa48db86b28
CodeSandbox <support@codesandbox.io>
2023-09-26 18:46:56 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,105 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import NotesIcon from "./assets/note_icon.png";
+import PlusIcon from "./assets/plus_icon.png";
+import DeleteIcon from "./assets/delete_icon.png";
+import "./App.scss";
+import dayjs from "dayjs";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [notes, setNotes] = useState<Array<Record<string, string>>>([]);
+  const [activeNote, setActiveNote] = useState(0);
+  const [activeNoteContent, setActiveNoteContent] = useState("");
+
+  const updateNotes = (updates: Array<Record<string, string>>) => {
+    setNotes([...updates]);
+  };
+
+  const addNote = () => {
+    const newNote = {
+      title: "",
+      content: "",
+      created_at: `${dayjs().format("ddd, DD MMMM YYYY")} at ${dayjs().format(
+        "hh:mm A",
+      )}`,
+    };
+    updateNotes([{ ...newNote }, ...notes]);
+    setActiveNote(0);
+  };
+
+  const deleteNote = (noteID: number) => {
+    notes.slice(noteID, 1);
+    updateNotes(notes);
+    if (activeNote >= noteID) {
+      setActiveNote(activeNote >= 1 ? activeNote - 1 : 0);
+    }
+  };
+
+  const handleChange = ({
+    target: { value },
+  }: {
+    target: { value: string };
+  }) => {
+    console.log(value);
+    if (notes.length === 0) return;
+    const header = value.split(/\r?\n/)[0];
+    if (notes.length !== 0 && notes[activeNote].title !== header) {
+      notes[activeNote].title = header;
+      notes[activeNote].content = value;
+      updateNotes([...notes]);
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
+    <div className="container">
+      <div className="container__left">
+        <div className="container__left__header">
+          <div className="container__left__header__title_and_logo">
+            <img src={NotesIcon} alt="Notes Icon" />
+            <p>My Notes</p>
+          </div>
+          <div className="container__left__header__action" onClick={addNote}>
+            <img src={PlusIcon} alt="Add New Note Icon" />
+            <p>New Note</p>
+          </div>
+        </div>
+        <div className="container__left__content">
+          {notes.map((item, index) => (
+            <div
+              key={`${item.title}_${index}`}
+              className={`container__left__content__row ${
+                index === activeNote && "active"
+              }`}
+              onClick={() => setActiveNote(index)}
+            >
+              <div className="container__left__content__row__left">
+                <p className="container__left__content__row__left__title">
+                  {item.title || "Untitled"}
+                </p>
+                <p className="container__left__content__row__left__date">
+                  {item.created_at}
+                </p>
+              </div>
+              <img
+                src={DeleteIcon}
+                alt="Delete Note Icon"
+                className="container__left__content__row__action"
+                onClick={() => deleteNote(index)}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
+      <div className="container__right">
+        <p className="container__right__date">
+          {notes[activeNote]?.created_at}
         </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
+        <textarea
+          name="note_input"
+          placeholder="Write Your Note Here"
+          onChange={handleChange}
+          value={notes[activeNote]?.content}
+        ></textarea>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
